0d9beeced0609868bc93b0d238255b51aedaf779
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2018-05-30 20:08:30 +0530

    .

--- microservices/api/src/__init__.py
+++ microservices/api/src/__init__.py
@@ -2,11 +2,11 @@ from flask import Flask
 import subprocess
 subprocess.Popen(["python","src/utils/clock.py"])
 
-api = Flask(__name__)
+app = Flask(__name__)
 
 # This line adds the hasura example routes form the hasura.py file.
 # Delete these two lines, and delete the file to remove them from your project
 from .hasura import hasura_examples
-api.register_blueprint(hasura_examples)
+app.register_blueprint(hasura_examples)
 
 from .server import *
--- microservices/api/src/server.py
+++ microservices/api/src/server.py
@@ -1,16 +1,16 @@
-from src import api
+from src import app
 from flask import Flask, render_template
 from flask import jsonify
 
-@api.route("/")
+@app.route("/")
 def home():
     return "Hello Parul :P"
 
-@api.route("/lang")
+@app.route("/lang")
 def index():
     return render_template("lang.html",langreport=[{'lang':'python','w1':100,'w2':4},{'lang':'python','w1':3,'w2':4}])
 # Uncomment to add a new URL at /new
 
-@api.route("/json")
+@app.route("/json")
 def json_message():
     return jsonify(message="Hello World")
