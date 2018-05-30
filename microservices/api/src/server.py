8cc41b5852ea441164df61a444ae173a9bd7b347
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2018-05-30 16:15:41 +0530

    .

--- microservices/api/src/server.py
+++ microservices/api/src/server.py
@@ -1,17 +1,17 @@
-from src import app
+from src import api
 from flask import Flask, render_template
 # from flask import jsonify
 from src.backend import *
 
-@app.route("/")
+@api.route("/")
 def home():
     return "Hasura Hello World"
 
-@app.route("/lang")
+@api.route("/lang")
 def index():
     return render_template("lang.html",langreport=[{'lang':'python','w1':100,'w2':4},{'lang':'python','w1':3,'w2':4}])
 # Uncomment to add a new URL at /new
 
-@app.route("/json")
+@api.route("/json")
 def json_message():
     return jsonify(message="Hello World")
