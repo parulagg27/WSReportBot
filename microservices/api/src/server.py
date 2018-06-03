e8192260ffe454a71acadebbde66f0ac36d102f5
Mulx10 <mehulkumarnirala@iitkgp.ac.in>
2018-06-03 18:08:24 +0530

    slack bot

--- microservices/api/src/server.py
+++ microservices/api/src/server.py
@@ -1,16 +1,74 @@
 import os
+import json
 import signal
+import requests
 import subprocess
 from src import app
-from flask import Flask, render_template
 from flask import jsonify
 from datetime import datetime
 from datetime import timedelta
-from src.utils.backend import get_weekly_report, get_language_report_week
 from src.utils.login import login
+from flask import jsonify, request
+from flask import Flask, render_template
+from src.utils.backend import get_weekly_report, get_language_report_week
+
+slackToken = os.environ['SLACK_TOKEN']
+botAccessToken = os.environ['BOT_ACCESS_TOKEN']
+hasuraDataUrl = "http://data.hasura/v1/query"
+chatUrl = "https://slack.com/api/chat.postMessage"
 
 headers = login('mehul','mehul@hasura')
 p = None
+
+def sendConfirmation(id, message, responseUrl):
+    payload = {
+        "text": "Showing You reports for last week",
+        "attachments": [
+            {
+                "text": '"'+message+'"',
+                "fallback": "You are indecisive",
+                "callback_id": id,
+                "color": "#3AA3E3",
+                "attachment_type": "default",
+                "actions": [
+                    {
+                        "name": "yes",
+                        "text": "Yep",
+                        "type": "button",
+                        "value": "yes"
+                    },
+                    {
+                        "name": "no",
+                        "text": "Nope",
+                        "type": "button",
+                        "value": "no"
+                    }
+                ]
+            }
+        ]
+    }
+    headers = {
+        'content-type': "application/json",
+    }
+
+    response = requests.request("POST", responseUrl, data=json.dumps(payload), headers=headers)
+    print(response.text)
+
+@app.route('/langslack', methods=['POST'])
+def event():
+    data = request.form.to_dict()
+    print(data)
+    print("SlackToken: " + slackToken)
+    receivedToken = data["token"]
+    print("ReceivedToken: " + receivedToken)
+    if (receivedToken==slackToken):
+        receivedMessage= data["text"]
+        id = "None"
+        sendConfirmation(id, receivedMessage, data["response_url"])
+        return "Waiting for confirmation"
+    else:
+        return "Invalid Token"
+
 @app.route("/")
 def home():
     return "Hello Parul :P"
